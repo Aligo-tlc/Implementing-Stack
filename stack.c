43ec7cbaa507fffb723203d5e916d2133625036a
Aligo-tlc <79133939+Aligo-tlc@users.noreply.github.com>
2021-07-22 01:16:04 +0300

    Create stack.c

---
+++ stack.c
@@ -0,0 +1,129 @@
+#include <stdio.h>  
+#include <stdlib.h>  
+void push();  
+void pop();  
+void display(); 
+
+//Create the node 
+struct node   
+{  
+int val;  
+struct node *next;  
+};  
+
+//Create node head
+struct node *head;  
+int main ()  
+{  
+    int choice=0; 
+	printf("\n**** Emmanuel Guya ***");    
+    printf("\n****Implementing Stack operations using linked list***\n");  
+    printf("\n----------------------------------------------");  
+    while(choice != 4)  
+    {  
+        printf("\nChoose one from the below options...\n");  
+        printf("\n1.Push\n2.Pop\n3.Show\n4.Exit");  
+        printf("\n Enter your choice\n");        
+        scanf("%d",&choice); 
+ //Use switch statement for option selections push, pop or display, exit
+        switch(choice)  
+        {  
+            case 1:  
+            {   
+                push();  
+                break;  
+            }  
+            case 2:  
+            {  
+                pop();  
+                break;  
+            }  
+            case 3:  
+            {  
+                display();  
+                break;  
+            }  
+            case 4:   
+            {  
+                printf("Exiting....");  
+                break;   
+            }  
+            default:  
+            {  
+                printf("Please Enter valid choice ");  
+            }   
+    };  
+}  
+} 
+
+//create the push function
+void push ()  
+{  
+    int val;  
+    struct node *ptr = (struct node*)malloc(sizeof(struct node));   
+    if(ptr == NULL)  
+    {  
+        printf("not able to push the element");   
+    }  
+    else   
+    {  
+        printf("Enter the value to push");  
+        scanf("%d",&val);  
+        if(head==NULL)  
+        {         
+            ptr->val = val;  
+            ptr -> next = NULL;  
+            head=ptr;  
+        }   
+        else   
+        {  
+            ptr->val = val;  
+            ptr->next = head;  
+            head=ptr;  
+               
+        }  
+        printf("Item pushed\n");  
+          
+    }  
+}  
+ 
+//create pop action 
+void pop()  
+{  
+    int item;  
+    struct node *ptr;  
+    if (head == NULL)  
+    {  
+        printf("Underflow\n");  
+    }  
+    else  
+    {  
+        item = head->val;  
+        ptr = head;  
+        head = head->next;  
+        free(ptr);  
+        printf("Item popped\n");  
+          
+    }  
+}
+
+//Display action stack elements  
+void display()  
+{    
+    struct node *ptr;  
+    ptr=head;  
+    if(ptr == NULL)  
+    {  
+        printf("Stack is empty\n");  
+    }  
+    else  
+    {  
+        printf("-----------------------------------------\n");
+		printf("Printing Stack elements \n");  
+        while(ptr!=NULL)  
+        {  
+            printf("%d\n",ptr->val);  
+            ptr = ptr->next;  
+        }  
+    }  
+}
